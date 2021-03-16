32c884f9b480278ed3c05969bc93b542109bd386
Eduardo Antón <eantonc@gmail.com>
2021-03-16 14:54:50 +0100

    Adds movidon to have check different if we have approved or not

--- contentScript.js
+++ contentScript.js
@@ -37,17 +37,29 @@ async function showApproved(element) {
   const project = commitLinkParts[4]
   const commitHash = commitLinkParts[6]
 
-  const response = await fetch(`https://bitbucket.org/!api/2.0/repositories/${workspace}/${project}/commit/${commitHash}?fields=participants`);
-  const result = await response.json();
+  const userResponse = await fetch(`https://bitbucket.org/!api/2.0/user?fields=account_id`);
+  const userResult = await userResponse.json();
+
+  let response = await fetch(`https://bitbucket.org/!api/2.0/repositories/${workspace}/${project}/commit/${commitHash}?fields=participants`);
+  let participantsResult = await response.json();
 
   let approvedCount = 0
-  if (result && result.participants) {
-    approvedCount = result.participants.filter(participant => !!participant.approved).length
+  let approvedByMe = false
+  if (participantsResult && participantsResult.participants && participantsResult.participants.length > 0) {
+    approvedCount = participantsResult.participants.filter(participant => !!participant.approved).length
+
+    const myParticipant = participantsResult.participants
+      .filter(participant => participant.user.account_id === userResult.account_id)[0]
+    approvedByMe = myParticipant ? myParticipant.approved : false
   }
   const divContainer = document.createElement('div')
   divContainer.id = 'approvedCount'
   divContainer.style = 'padding: 0 1rem; display: flex; align-items: center; justify-content: center;'
-  divContainer.innerHTML = `<span class="dCzFuw" role="img"><svg width="18" height="18" viewBox="0 0 24 24" focusable="false" role="presentation"><g fill-rule="evenodd"><circle fill="currentColor" cx="12" cy="12" r="10"></circle><path d="M9.707 11.293a1 1 0 1 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 1 0-1.414-1.414L11 12.586l-1.293-1.293z" fill="inherit"></path></g></svg></span>`
+  if (approvedByMe) {
+    divContainer.innerHTML = `<span class="dCzFuw" role="img"><svg width="18" height="18" viewBox="0 0 24 24" focusable="false" role="presentation"><g fill-rule="evenodd"><circle fill="currentColor" cx="12" cy="12" r="10"></circle><path d="M9.707 11.293a1 1 0 1 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 1 0-1.414-1.414L11 12.586l-1.293-1.293z" fill="inherit"></path></g></svg></span>`
+  } else {
+    divContainer.innerHTML = `<span class="dCzFuw" role="img" style="color: grey;"><svg width="18" height="18" viewBox="0 0 24 24" focusable="false" role="presentation"><g fill-rule="evenodd"><circle fill="currentColor" cx="12" cy="12" r="10"></circle><path d="M9.707 11.293a1 1 0 1 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 1 0-1.414-1.414L11 12.586l-1.293-1.293z" fill="inherit"></path></g></svg></span>`
+  }
   const spanCount = document.createElement('span')
   spanCount.innerText = approvedCount
   divContainer.prepend(spanCount)
